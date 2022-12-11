17867105b1b287e291e72fc81fe8bc800d4f508f
Nikita Borovkov <borovckov@yandex.ru>
2022-12-11 21:11:22 +0300

    add modal

---
+++ UI/elements/AppModal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="modal" class="layout" @click="$emit('close-modal')">
+      <div class="inner">
+        <slot style="width: 100%;"></slot>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppModal",
+        props: {
+          modal: {
+            type: Boolean,
+            required: true
+          }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  .layout {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background-color: rgba(0,0,0,.5);
+  }
+  .inner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 60%;
+  }
+</style>
--- pages/AboutMe.vue
+++ pages/AboutMe.vue
@@ -55,15 +55,17 @@
     <divider text="Квалификация"/>
 
     <div>
-      <div style="display: flex; justify-content:center; margin: auto; flex-wrap: wrap">
-        <div v-for="diploma in qualification" :key="diploma.id" style="width: 350px; margin: 10px;">
-          <img
-            style="width: 100%; display: block; margin: 10px;"
-            :src="diploma.scan" alt="diploma_img"
-          >
+      <div class="flex-center">
+        <div @click="openDiploma(diploma.id)" class="diploma" v-for="diploma in qualification" :key="diploma.id">
+          <img class="diploma__img" :src="diploma.scan" alt="diploma_img">
         </div>
       </div>
     </div>
+
+
+    <app-modal :modal="diplomaModal" @close-modal="diplomaModal = false">
+      <img style="width: 100%;" :src="selectedDiploma.scan" alt="diploma_img">
+    </app-modal>
   </div>
 </template>
 
@@ -71,17 +73,26 @@
   import diploma1 from '../assets/images/diplomas/diploma_1.jpg'
   import diploma2 from '../assets/images/diplomas/diploma_2.jpg'
   import Divider from "../UI/elements/Divider";
+  import AppModal from "../UI/elements/AppModal";
 
   export default {
     name: "AboutMe",
     layout: 'BasePageLayout',
-    components: {Divider},
+    components: { Divider, AppModal },
     data() {
       return {
         qualification: [
           {id: 0, scan: diploma1},
           {id: 1, scan: diploma2},
-        ]
+        ],
+        diplomaModal: false,
+        selectedDiploma: ''
+      }
+    },
+    methods: {
+      openDiploma(id) {
+        this.selectedDiploma = this.qualification.find((item) => item.id === id)
+        this.diplomaModal = true
       }
     }
   }
@@ -132,5 +143,14 @@
     -webkit-border-radius: 20px;
     -moz-border-radius: 20px;
     border-radius: 20px;
+  }
+  .diploma{
+    width: 350px;
+    display: block;
+    margin: 10px;
+    cursor: pointer;
+    &__img{
+      width: 100%;
+    }
   }
 </style>
